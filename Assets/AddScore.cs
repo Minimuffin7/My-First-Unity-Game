9cd88858bdd4a708e24b6322a12503cb720f68c7
Praktikantur1 <praktikantur1@klintra.fo>
2023-03-29 10:16:09 +0100

    Hello World

---
+++ Assets/AddScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    public SoundManager instance;
+
+    public AudioClip scoreSound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Score.score++;
+        instance.PlaySound(scoreSound);
+    }
+}
---
+++ Assets/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    void Start()
+    {
+        // Hide the canvas at the start of the game
+        gameObject.GetComponent<CanvasGroup>().alpha = 0;
+    }
+
+    void OnPlayerDeath()
+    {
+        // Show the canvas when the player dies
+        gameObject.GetComponent<CanvasGroup>().alpha = 1;
+    }
+}
